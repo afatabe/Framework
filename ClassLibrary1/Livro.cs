4e350c53d4950dc56f0931244b71c69286acd8fe
anderson f antonio <gilafa@hotmail.com>
2016-03-09 17:49:07 -0300

    OK

---
+++ ClassLibrary1/Livro.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Devmedia.Estudo.Sample.Library;
+
+namespace Devmedia.Estudo.Sample
+{
+    public class Livro
+    {
+        string titulo;
+        public string Titulo
+        {
+            get {return titulo;}
+            set {titulo = value;}
+        }
+
+        string autor;
+        public string Autor
+        {
+            get {return autor;}
+            set {autor = value;}
+        }
+
+        int anoPublicacao;
+        public string AnoPublicacao
+        {
+            get {return autor;}
+            set {autor = value;}
+        }
+
+        Biblioteca _biblioteca;
+        public Biblioteca Biblioteca1
+        {
+            get { return _biblioteca; }
+            set { _biblioteca = value; }
+        }
+
+
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using Devmedia.Estudo.Sample;
+
+namespace Devmedia.Estudo.FrameworkUIConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello world");
+            Console.ReadKey();
+
+            Livro livro = new Livro();
+
+            
+        }
+    }
+}
